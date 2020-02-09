512f35785a9671c452278f0646b6cf7c5d428960
Vanshika Sikarwar <vanshikasikawrar79@gmail.com>
2020-02-09 23:20:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                 startActivity(intent);
+                 finish();
+            }
+            },SPLASH_SCREEN);
+    }
+}
